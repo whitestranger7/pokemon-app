6e9738f08eb385b5eba0f4aead27938d68c2d5bc
Ivan <whitestranger7@gmail.com>
2020-01-19 18:03:48 +0300

    add: restructuring & add features to user auth

--- api/models/model.User.js
+++ api/models/model.User.js
@@ -1,7 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const userSchema = require('./model.User');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: value => {
+            if (!validator.isEmail(value)) {
+                throw new Error({error: 'Invalid Email address'})
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 7
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    timestamps: true
+});
 
 userSchema.methods.generateAuthToken = async function() {
     const user = this;
@@ -36,6 +68,6 @@ userSchema.pre('save', async function (next) {
     next()
 });
 
-const User = mongoose.model('User', userSchema);
+// const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = userSchema;
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const User = require('../models/User')
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-//Display all users
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
+});
+
+router.post('/users/create', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
     try {
-        const users = await User.find({});
-        res.status(200).send(users);
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token });
     } catch (error) {
         res.status(400).send(error);
     }
 });
 
-// Create a new user
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+router.post('/users/login', async (req, res) => {
     try {
-        await user.save()
-        // const token = await user.generateAuthToken()
-        res.status(201).send(user);
-    } catch (error) {
-        res.status(400).send(error);
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.status(200).send({ user, token });
+    } catch(error) {
+        res.status(400).send('Unable to login');
     }
 });
 
