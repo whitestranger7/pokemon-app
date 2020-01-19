619c089d284acc9aa532350de6c988af7c1ebc45
Ivan <whitestranger7@gmail.com>
2020-01-19 18:05:17 +0300

    rebuild: restructure user model

--- api/models/model.User.js
+++ api/models/model.User.js
@@ -35,39 +35,4 @@ const userSchema = mongoose.Schema({
     timestamps: true
 });
 
-userSchema.methods.generateAuthToken = async function() {
-    const user = this;
-    const token = jwt.sign({ _id: user._id.toString() }, 'grodnopokemon2016');
-
-    user.tokens = user.tokens.concat({ token });
-    await user.save();
-
-    return token;
-};
-
-userSchema.statics.findByCredentials = async (email, password) => {
-
-    const user = await User.findOne({email});
-    if(!user) {
-        throw new Error('Unable to login');
-    };
-
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if(!isPasswordMatch) {
-        throw new Error('Unable to login');
-    };
-
-    return user;
-};
-
-userSchema.pre('save', async function (next) {
-    const user = this
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
-    next()
-});
-
-// const User = mongoose.model('User', userSchema);
-
 module.exports = userSchema;
